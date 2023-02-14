866104e0227a5238ce4cd35bb67fb99f571e23ef
StefanioHabrArticles <diggerss13@yandex.ru>
2023-02-15 01:33:17 +0300

    fix

--- PolymorphicContracts.Models/Animals/Animal.cs
+++ PolymorphicContracts.Models/Animals/Animal.cs
@@ -2,6 +2,6 @@ using System.Text.Json.Serialization;
 
 namespace PolymorphicContracts.Models.Animals;
 
-[JsonDerivedType(typeof(Cat), nameof(Cat))]
-[JsonDerivedType(typeof(Dog), nameof(Dog))]
+[JsonDerivedType(typeof(Cat), typeDiscriminator: nameof(Cat))]
+[JsonDerivedType(typeof(Dog), typeDiscriminator: nameof(Dog))]
 public abstract record Animal;
--- PolymorphicContracts.Models/Fruits/Fruit.cs
+++ PolymorphicContracts.Models/Fruits/Fruit.cs
@@ -2,7 +2,7 @@ using System.Text.Json.Serialization;
 
 namespace PolymorphicContracts.Models.Fruits;
 
-[JsonDerivedType(typeof(Apple), 0)]
-[JsonDerivedType(typeof(Citrus), 1)]
-[JsonDerivedType(typeof(Grape), 2)]
+[JsonDerivedType(typeof(Apple), typeDiscriminator: 0)]
+[JsonDerivedType(typeof(Citrus), typeDiscriminator: 1)]
+[JsonDerivedType(typeof(Grape), typeDiscriminator: 2)]
 public abstract record Fruit;
